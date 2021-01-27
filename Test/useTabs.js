c6099fce1215af604abd94887efb275cfa73b063
netLee <wltjr1426@naver.com>
2021-01-27 15:12:01 +0900

    Update annotation

--- Test/useTabs.js
+++ Test/useTabs.js
@@ -1,3 +1,6 @@
+// useState 연습
+// setState는 모든 걸 새로고침 (re-render), render function이 없다고 render이 안되는 것은 아님
+
 import React, { useState } from "react";
 import "./styles.css";
 
@@ -13,18 +16,23 @@ const content = [
 ];
 
 const useTabs = (initialTab, allTabs) => {
+  // 만약 allTabs가(ture)가 아니거나 allTabs가 배열이 아니라면
   if (!allTabs || !Array.isArray(allTabs)) {
     return;
   }
   const [currentIndex, setCurrentIndex] = useState(initialTab);
   return {
     currentItem: allTabs[currentIndex], // 현재 index의 모든 원소를 가져옴 (배열 형태로)
+    // setCurrentIndex를 통해 state(useState를 통해) 바꿔주고,
+    // 그를 통해 currentItem의 currentIndex를 바꿔주면서 새로고침 하는 것!
+
     chageItem: setCurrentIndex, // setCuurentIndex를 통해 content의 배열을 바꿈
+    // setCurrentIndex를 통해 state를 업데이트 시키는 것!!
   };
 };
 
 const App = () => {
-  const { currentItem, chageItem } = useTabs(0, content);
+  const { currentItem, chageItem } = useTabs(0, content); // 초기 값은 배열의 0번째(처음) 실행
   return (
     <div className="App">
       <h1>Hello</h1>
