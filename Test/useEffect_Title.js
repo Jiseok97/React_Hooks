27accb5bb66b437ca0c7d6ef769593c38dfc1444
netLee <wltjr1426@naver.com>
2021-01-29 12:35:41 +0900

    Update annotation

--- Test/useEffect_Title.js
+++ Test/useEffect_Title.js
@@ -7,12 +7,15 @@ const useTitle = (initialTitle) => {
     const htmlTitle = document.querySelector("title");
     htmlTitle.innerText = title;
   };
+  // useEffect(componentDidMount, componentWillUpdate)
   useEffect(updateTitle, [title]);
+  // 제목을 업데이트 할 수 있게 return 해줌
   return setTitle;
 };
 
 const App = () => {
   const titleUpdater = useTitle("Loading...");
+  // 5초 뒤에 title을 Home으로 update
   setTimeout(() => titleUpdater("Home"), 5000);
   return (
     <div className="App">
