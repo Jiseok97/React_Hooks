3b8727c8981a66a9a214a80a264e5ff015c10b5a
netLee <wltjr1426@naver.com>
2021-01-30 16:35:31 +0900

    Update annotation

--- useConfirm/useConfirm.js
+++ useConfirm/useConfirm.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./styles.css";
 
+// Not require Hooks
+// useConfirm은 사용자가 무언가를 하기 전에 확인 하는 것
+// ex) 사용자가 버튼을 크릭하는 작업을 하면 (이벤트 실행 전) 메세지 보여주기
+// 어떤 원리 인가?
+// browser는 우리가 어떤걸 저장, 삭제할 때, 일단 그걸 막고
+// browser는 confirm을 하고, 우리가 확인을 하면 function을 계속 진행
+
 const useConfirm = (message = "", callback, rejection) => {
   if (typeof callback !== "function") {
     return;
